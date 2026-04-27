a6312c760e12406a5d696a56ef28fd838e6664f5
ketsara <ketsara.piy@ku.th>
2026-04-27 11:17:48 +0700

    [Added]: เพิ่ม Test Step กรอก email ใน Test Func Login Page: Success ในไฟล์ Login.spec.ts

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -3,8 +3,11 @@ import { test, expect } from '@playwright/test';
 test('Login Page: Success', async ({ page }) => {
   await test.step('เข้าสู่หน้าเว็บไซต์ ไปหน้า Login', async() =>{
     await page.goto('/')
-    await page.getByRole('link', { name: 'Login Form Practice login'}).click
+    await page.getByRole('link', { name: 'Login Form Practice login'}).click()
   });
 
+  await test.step('กรอก email', async() =>{
+    await page.getByTestId('email-input').fill('user@company.com')
+  });
 });
 

4b45edc068fad6e94dc70fbfb72eba01b65714f4
ketsara <ketsara.piy@ku.th>
2026-04-28 15:06:15 +0700

    [Added]: เพิ่ม Test Func: กรอก Tax Invoice จำนวน 1 ชิ้น สำเร็จ ใน InvoiceForm.spec.ts

--- tests/InvoiceForm.spec.ts
+++ tests/InvoiceForm.spec.ts
@@ -1,5 +1,76 @@
 import { test, expect } from '@playwright/test';
 
+test('กรอก Tax Invoice จำนวน 1 ชิ้น สำเร็จ', async ({ page }) => {
+  await test.step('เข้าสู่หน้าเว็บไซต์ ไปหน้า Invoice Form', async() =>{
+    await page.goto('/')
+    await page.getByRole('link', { name: 'Invoice Form'}).click()
+  });
+
+  await test.step('เลือก Document type เป็น Tax Invoice', async() =>{
+    await page.getByTestId('invoice-type-select').selectOption({label: 'Tax Invoice'})
+  });
+
+  await test.step('กรอก Item name เป็น C-Level Consulting', async() =>{
+    await page.getByTestId('row-1-name').fill('C-Level Consulting')
+  });
+
+  await test.step('กรอก Category เป็น Con', async() =>{
+    await page.getByTestId('row-1-category').fill('Con')
+  });
+
+  await test.step('เลือก Consulting', async() =>{
+    await page.getByTestId('row-1-category-option-0').click()
+  });
+
+  await test.step('กรอก Unit price เป็น 2,000', async() =>{
+    await page.getByTestId('row-1-unit-price').fill('2,000')
+  });
+
+  await test.step('กรอก Qty เป็น 4', async() =>{
+    await page.getByTestId('row-1-qty').clear()
+    await page.getByTestId('row-1-qty').fill('4')
+  });
+
+  await test.step('ตรวจ Total เป็น 8,000.00', async() =>{
+    await page.locator('body').click()
+    await expect(page.getByTestId('row-1-total')).toHaveValue('8,000.00')
+  });
+
+  await test.step('เปลี่ยน Total เป็น 6,500.00', async() =>{
+    await page.getByTestId('row-1-total').clear()
+    await page.getByTestId('row-1-total').fill('6,500')
+  });
+
+  await test.step('ตรวจ Total เป็น 6,500.00', async() =>{
+    await page.locator('body').click()
+    await expect(page.getByTestId('row-1-total')).toHaveValue('6,500.00')
+  });
+
+  await test.step('ตรวจ Subtotal เป็น 6,500.00', async() =>{
+    await expect(page.getByTestId('summary-subtotal')).toHaveText('6,500.00')
+  });
+
+  await test.step('ตรวจ VAT (7%) เป็น 455.00', async() =>{
+    await expect(page.getByTestId('summary-vat')).toHaveText('455.00')
+  });
+
+  await test.step('ตรวจ Grand total เป็น 6,955.00', async() =>{
+    await expect(page.getByTestId('summary-grand-total')).toHaveText('6,955.00')
+  });
+
+  await test.step('กด Submit document', async() =>{
+    await page.getByTestId('submit-button').click()
+  });
+
+  await test.step('ตรวจสอบ URL เท่ากันกับ https://ui-sandbox-omega.vercel.app/invoice/success', async() =>{
+    await expect(page).toHaveURL('/invoice/success')
+  });
+
+  await test.step('ตรวจสอบข้อความบนหน้าเว็บ Submit Invoice Successful', async() =>{
+    await expect(page.getByTestId('success-heading')).toHaveText('Submit Invoice Successful')
+  });
+});
+
 test('กรอก Receipt จำนวน 2 ชิ้น สำเร็จ', async ({ page }) => {
   await test.step('เข้าสู่หน้าเว็บไซต์ ไปหน้า Invoice Form', async() =>{
     await page.goto('/')

3bf87dbcc8197e6f2a0dd752b4b584111d04a1e5
ketsara <ketsara.piy@ku.th>
2026-04-28 11:20:53 +0700

    [Added]: เพิ่ม Test Step: เข้าสู่หน้าเว็บไซต์ ไปหน้า Invoice Form ใน Test Func กรอก Receipt จำนวน 2 ชิ้น สำเร็จ ใน InvoiceForm.spec.ts

--- tests/InvoiceForm.spec.ts
+++ tests/InvoiceForm.spec.ts
@@ -1,5 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('กรอก Receipt จำนวน 2 ชิ้น สำเร็จ', async ({ page }) => {
-
+  await test.step('เข้าสู่หน้าเว็บไซต์ ไปหน้า Invoice Form', async() =>{
+    await page.goto('/')
+    await page.getByRole('link', { name: 'Invoice Form'}).click()
+  });
 });
